cab421e5b110eceee020f2ac1b8322a934eaefb1
Lucas <lucas_farias6@outlook.com>
2022-10-28 22:38:11 -0300

    Initial commit

---
+++ app/src/main/java/com/lucasfariassa/divideconta/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lucasfariassa.divideconta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
